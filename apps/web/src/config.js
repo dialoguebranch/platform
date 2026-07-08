37b45ccc721453d349d29486599c1a1f8bb04dac
Dennis Hofs <d.hofs@connectedcare.nl>
2026-07-08 13:10:47 +0200

    #61: moved hard-coded client config to env

--- apps/web/src/config.js
+++ apps/web/src/config.js
@@ -1,10 +1,10 @@
 const config = {
-    "baseUrl" : "http://localhost:8089/dlb-web-service/v1",
-    "logLevel" : "1",
-    "keycloak" : {
-        "url" : "http://localhost:8081",
-        "realm" : "dialoguebranch",
-        "clientId" : "dlb-web-service"
+    "baseUrl": import.meta.env.VITE_DLB_API_BASE_URL ?? "http://localhost:8089/dlb-web-service/v1",
+    "logLevel": import.meta.env.VITE_DLB_LOG_LEVEL ?? "1",
+    "keycloak": {
+        "url": import.meta.env.VITE_DLB_AUTH_KEYCLOAK_URL ?? "http://localhost:8081",
+        "realm": import.meta.env.VITE_DLB_AUTH_KEYCLOAK_REALM ?? "dialoguebranch",
+        "clientId": import.meta.env.VITE_DLB_AUTH_KEYCLOAK_CLIENT_ID ?? "dlb-web-service"
     }
 };
 
