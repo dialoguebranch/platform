d999aeba0cee2f60309087a971dd12ab71172b0d
Harm op den Akker <hopdenakker@fruittreelabs.com>
2026-07-08 14:01:45 +0100

    Project language map fix
    Retrieving project data now returns language mappings

--- apps/api/src/main/java/com/dialoguebranch/web/service/storage/model/DBProject.java
+++ apps/api/src/main/java/com/dialoguebranch/web/service/storage/model/DBProject.java
@@ -67,8 +67,7 @@ public class DBProject {
 	@JoinColumn(name = "latest_version_id")
 	private DBProjectVersion latestVersion;
 
-	@OneToMany(mappedBy = "project")
-	@JsonIgnore
+	@OneToMany(mappedBy = "project", fetch = FetchType.EAGER)
 	private Set<DBProjectLanguageMapping> languageMappings = new HashSet<>();
 
 	@OneToMany(mappedBy = "project")
